abe07fedf120df59a2ab19b0048f010ba35f0ed2
MonoDG <monodg@proton.me>
2024-01-28 13:41:23 -0500

    Add reverse passing test and function

--- index.js
+++ index.js
@@ -2,4 +2,12 @@ function capitalize(string) {
     return string[0].toUpperCase() + string.slice(1);
 }
 
-export default capitalize;
+function reverse(string) {
+    let reversedString = [];
+    for (let i = 0; i < string.length; i++) {
+        reversedString.push(string[string.length - 1 - i]);
+    }
+    return reversedString.join("");
+}
+
+export { capitalize, reverse };
--- index.test.js
+++ index.test.js
@@ -1,6 +1,11 @@
-import capitalize from "./index";
+import { capitalize, reverse } from "./index";
 
 it("Should capitalize string", () => {
     expect(capitalize("hello world")).toBe("Hello world");
     expect(capitalize("fizz")).toBe("Fizz");
+});
+
+it("Should reverse string", () => {
+    expect(reverse("hello")).toBe("olleh");
+    expect(reverse("fizz buzz")).toBe("zzub zzif");
 });
